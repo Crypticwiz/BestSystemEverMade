ed5315a6c47309987c7c44c37e3da95e877b5d58
Matthew Brooks <mbrooks7@email.arizona.edu>
2019-03-19 11:55:31 -0700

    ADC is good

--- Brooks Lab 4/src/adc.cpp
+++ Brooks Lab 4/src/adc.cpp
@@ -10,30 +10,25 @@ void initADC0(){
   ADMUX &= ~(1 << ADLAR);
 
   // Set ADC0 as single-ended input with MUX[5:0] = 0b000000
-  ADMUX &= ~(1 << MUX2) | (1 << MUX1) | (1 << MUX0) | (1<< MUX3) | (1<<MUX4);
+  ADMUX &= ~(1 << MUX2 |  1<< MUX1 | 1 << MUX0 | 1<< MUX3 | 1<<MUX4); //
   ADCSRB &= ~(1<<MUX5);
 
 
   // set Auto Trigger Source Selection
-  // set to free-running mode ADTS[2:0] = 0b000
-  // 
-  ADCSRB &= ~(1 << ADTS2 | 1 << ADTS1 | 1 << ADTS0);
-
-  // enable auto-triggering turn-on ADC
-  ADCSRA |= (1 << ADATE);
 
   //Set event to trigger ADC sample
+  //Set Free-Running Mode ADST[2:0] = 0b00
   ADCSRB &= ~(1<<ADTS2 | 1<<ADTS1 | 1<<ADTS0); //Change as needed
   //Currently in free-running mode
 
   //Enable Interrupt Flag
-  ADCSRA |= (1<<ADIF);
+  //ADCSRA |= (1<<ADIF);
 
-  //Turn on ADC
-  ADCSRA |= (1 << ADEN);
+  //Turn on ADC  // enable auto-triggering turn-on ADC
+  ADCSRA |= (1 << ADEN | 1<<ADATE);
   // set the pre-scaler to 128
   // ADC clock frequency is 16 Mhz divided by pre-scaler = 125KHz
-  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  ADCSRA |= (1 << ADPS2 | 1 << ADPS1 | 1 << ADPS0);
 
   // disable ADC0 pin digital input - pin A0 on board
   DIDR0 |= (1 << ADC0D);
