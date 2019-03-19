98dcfef8753d2ff8f4a555a3f4b1e458e7374e67
Matthew Brooks <mbrooks7@email.arizona.edu>
2019-03-19 13:39:38 -0700

    Should be good

--- Brooks Lab 4/src/main.cpp
+++ Brooks Lab 4/src/main.cpp
@@ -16,23 +16,73 @@
 #include "switch.h"
 
 typedef enum stateType_enum {
-  Motor_On , devounce_On, Motor_Off, bebounce_Off
+  Motor_On , debounce_On, Motor_Off, debounce_Off
 }stateType;    //Set states
 
 volatile stateType state = Motor_On; //Set initial state of the system
 
+//Global Variable
+int on = 1;
+
 
 
 int main(){
- initADC0();
  initTimer1();
- initSwitchPB3():
- updateDutyCycle();
-
+ initSwitchPB3();
+sei();
 
   while(1){
+      if (on == 0)
+      {
+        OCR3A = 0;
+        OCR4A = 0;
+      }
+      else 
+      {
+        initADC0();
+        updateDutyCycle();
+      }
 
+    switch (state) {
+        case Motor_On:
+        break;
+
+        case debounce_On:
+        delayUs(100);
+        state = Motor_Off;
+        break;
+
+        case Motor_Off:
+        break;
+
+        case debounce_Off:
+        delayUs(100);
+        state = Motor_On;
+        break;
+
+        default :
+        state = Motor_On;
+
+
+    }
   }
 
   return 0;
+}
+
+ISR(PCINT0_vect){
+    if(state == Motor_On){
+      state = debounce_On;
+    }
+    else if (state == Motor_Off){
+      if (on == 1){
+        on = 0;
+      }
+      else {
+        on = 1;
+      }
+      state = debounce_Off;
+    }
+
+
 }
