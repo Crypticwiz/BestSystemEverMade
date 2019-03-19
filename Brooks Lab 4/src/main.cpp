25f7665803b559ce285873395e343b62fe5cd427
Matthew Brooks <mbrooks7@email.arizona.edu>
2019-03-19 12:55:36 -0700

    WOrk in progress

--- Brooks Lab 4/src/main.cpp
+++ Brooks Lab 4/src/main.cpp
@@ -7,9 +7,27 @@
 // program. It should never contain assignments to special function registers
 // for the exception key one-line code such as checking the state of the pin.
 //
+#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "pwm.h"
+#include "timer.h"
+#include "adc.h"
+#include "switch.h"
+
+typedef enum stateType_enum {
+  Motor_On , devounce_On, Motor_Off, bebounce_Off
+}stateType;    //Set states
+
+volatile stateType state = Motor_On; //Set initial state of the system
+
 
 
 int main(){
+ initADC0();
+ initTimer1();
+ initSwitchPB3():
+ updateDutyCycle();
 
 
   while(1){
--- Brooks Lab 4/src/pwm.cpp
+++ Brooks Lab 4/src/pwm.cpp
@@ -6,3 +6,16 @@ void updateDutyCycle(){
     OCR3A = 255*( ADCL + (ADCH << 0x08) /1024);
     OCR4A = 255*( 1 - (ADCL + (ADCH << 0x08) /1024) );
 }
+
+void initTimer3(){
+TCCR3A |= (1 << WGM30); // Set timer to be in Fast PWM, 8-bit mode 
+TCCR3A &= ~(1 << WGM31);
+TCCR3B |= (1 << WGM32);
+TCCR3B &= ~(1<< WGM33);
+
+TCCR3B |= (1 << CS30); // Sets the prescaler to 1
+  TCCR3B &= ~(1<< CS31);
+  TCCR3B &= ~(1<< CS32);
+
+  OCR3A = 16;     
+}
